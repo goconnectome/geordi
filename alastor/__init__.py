cf2c7728e3fda7a1f12e9d68a5c2edfd7d9c026e
Brodie Rao <brodie@sf.io>
2012-05-01 11:47:38 -0700

    Refactor request simulation

--- alastor/__init__.py
+++ alastor/__init__.py
@@ -7,47 +7,51 @@ from django.conf import settings
 from django.http import HttpResponse
 from django.test.client import Client
 
-def profile(method, path, data, headers, gprof2dot='gprof2dot'):
-    client = Client()
-    callback = {'GET': client.get, 'POST': client.post, 'HEAD': client.head,
-                'OPTIONS': client.options, 'PUT': client.put,
-                'DELETE': client.delete}[method]
+class SimulatedRequest(object):
+    def __init__(self, request):
+        self._method = request.method
+        self._headers = dict([(k, v) for (k, v) in request.META.iteritems()
+                              if k.startswith('HTTP_')])
+        self._data = dict((k, request.POST[k]) for k in request.POST)
 
-    profiler = cProfile.Profile()
-    profiler.runcall(callback, path, data, **headers)
-    profiler.create_stats()
+        path = request.path
+        query = request.GET.copy()
+        query.pop('__alastor__', None)
+        query = query.urlencode()
+        if query:
+            path += '?' + query
+        self._path = path
 
-    with tempfile.NamedTemporaryFile() as stats:
-        stats.write(marshal.dumps(profiler.stats))
-        stats.flush()
-        proc = subprocess.Popen('%s -f pstats %s | dot -Tpdf' # XXX
-                                % (gprof2dot, stats.name),
-                                shell=True, stdin=subprocess.PIPE,
-                                stdout=subprocess.PIPE)
-        output = proc.communicate()[0]
-        if proc.poll():
-            raise Exception # XXX
+    def profile(self, gprof2dot='gprof2dot'):
+        client = Client()
+        callback = {'GET': client.get,
+                    'POST': client.post,
+                    'HEAD': client.head,
+                    'OPTIONS': client.options,
+                    'PUT': client.put,
+                    'DELETE': client.delete}[self._method]
 
-    return output
+        profiler = cProfile.Profile()
+        profiler.runcall(callback, self._path, self._data, **self._headers)
+        profiler.create_stats()
 
-def distill(request):
-    headers = dict([(k, v) for (k, v) in request.META.iteritems()
-                    if k.startswith('HTTP_')])
-
-    path = request.path
-    query = request.GET.copy()
-    query.pop('__alastor__', None)
-    query = query.urlencode()
-    if query:
-        path += '?' + query
-
-    data = dict((k, request.POST[k]) for k in request.POST)
-
-    return request.method, path, data, headers
+        with tempfile.NamedTemporaryFile() as stats:
+            stats.write(marshal.dumps(profiler.stats))
+            stats.flush()
+            proc = subprocess.Popen('%s -f pstats %s | dot -Tpdf' # XXX
+                                    % (gprof2dot, stats.name),
+                                    shell=True, stdin=subprocess.PIPE,
+                                    stdout=subprocess.PIPE)
+            output = proc.communicate()[0]
+            if proc.poll():
+                raise Exception # XXX
+        return output
 
 if getattr(settings, 'ALASTOR_CELERY', False):
     from celery.task import task
-    profiletask = task(profile)
+    @task
+    def profiletask(srequest, gprof2dot):
+        return srequest.profile(gprof2dot)
 else:
     profiletask = None
 
@@ -56,12 +60,11 @@ class AlastorMiddleware(object):
         if ('__alastor__' in request.GET and
             (request.user.is_superuser or settings.DEBUG)):
             gprof2dot = getattr(settings, 'ALASTOR_GPROF2DOT', 'gprof2dot')
-            method, path, data, headers = distill(request)
+            srequest = SimulatedRequest(request)
 
             if profiletask:
-                result = profiletask.delay(method, path, data, headers,
-                                           gprof2dot)
+                result = profiletask.delay(srequest, gprof2dot)
                 output = result.get() # XXX
             else:
-                output = profile(method, path, data, headers, gprof2dot)
+                output = srequest.profile(gprof2dot)
             return HttpResponse(output, content_type='application/pdf')
